67204f37f4f5ab2b1f49fcd70a02a935a96fb83d
Mikhail Merkulov <mihail.merkulov@gmail.com>
2012-10-24 14:04:46 +0200

    Define to check if the object is not nil and is not equal to NSNull

--- YTFExtensions/NSNull+Helper.h
+++ YTFExtensions/NSNull+Helper.h
@@ -8,3 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+/*
+ * Define to check if the object is not nil and is not equal to NSNull
+ * Example:
+ *     id obj = [json valueForKeyPath:....];
+ *     if (IS_NOTNIL_NOTNULL(obj)) {
+ *     ... }
+ */
+#define IS_NOTNIL_NOTNULL(x) x != nil && (NSNull *)x != [NSNull null]
+
