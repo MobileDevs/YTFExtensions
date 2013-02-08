efb9122433561866245d8dca38111ee125aa3d27
YetiTheFoot <reg.yeti@gmail.com>
2013-02-08 15:42:17 +0200

    clean up

---
+++ YTFExtensions/UIPinchGestureRecognizer_Initials.h
@@ -0,0 +1,13 @@
+//
+//  UIPinchGestureRecognizer_Initials.h
+//  YTFExtensions
+//
+//  Created by yeti on 18.01.13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPinchGestureRecognizer ()
+
+@end
