4cf0cdddfa63f4f5a6d1fb18764f3de2c2898f4f
YetiTheFoot <reg.yeti@gmail.com>
2013-02-13 12:48:14 +0200

    add wideheader for fast including

---
+++ YTFExtensions/YTFExtensions.h
@@ -0,0 +1,40 @@
+//
+//  Header.h
+//  YTFExtensions
+//
+//  Created by yeti on 13.02.13.
+//
+//
+
+#ifndef YTFExtensions_Header_h
+#define YTFExtensions_Header_h
+
+
+#import "NSArray+Helper.h"
+#import "NSData+Helper.h"
+#import "NSDate+Helper.h"
+#import "NSDictionary+Helper.h"
+#import "NSNull+Helper.h"
+#import "NSObject+Helper.h"
+#import "NSSet+Helper.h"
+#import "NSString+Helper.h"
+#import "NSURL+Helper.h"
+#import "UIAlertView+Helper.h"
+#import "UIApplication+Helper.h"
+#import "UIColor+Helper.h"
+#import "UIDevice+Helper.h"
+#import "UIImage+Helper.h"
+#import "UIImage+Alpha.h"
+#import "UIImage+Resize.h"
+#import "UIImage+RoundedCorner.h"
+#import "UITableView+Helper.h"
+#import "UITableViewContentUnavailableView.h"
+#import "UITableViewController+Helper.h"
+#import "UITableViewUpdatingView.h"
+#import "UIViewController+Helper.h"
+#import "UIView+Helper.h"
+#import "NSDictionary+URLParametrs.h"
+#import "NSString+HTML.h"
+#import "NSDate+TimeZones.h"
+
+#endif
